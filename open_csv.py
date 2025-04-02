2ed4ccec88570f1c4c16da2159abeabd294d100f
raghav-ri <raghavissa701@gmail.com>
2025-04-03 01:08:16 +0530

    Replacing Null Values

--- open_csv.py
+++ open_csv.py
@@ -8,6 +8,7 @@ print(df.columns)
 print(df.shape)
 print(df.dtypes)
 print(df.isnull().sum())
+print(df.isnull().sum().sum())
 
 #  0   ID                                3000 non-null   int64  
 #  1   Name                              3000 non-null   object 
@@ -22,9 +23,34 @@ print(df.isnull().sum())
 #  10  Expense Covered by Insurance (%)  2700 non-null   float64
 #  11  Government Hospital Use           3000 non-null   object
 #  12  BMI                               2700 non-null 
+df.drop_duplicates(inplace=True)
+df.loc[:,'Age']=df['Age'].fillna(df['Age'].mean())
+df.loc[:,'Hospital Visits Per Year']=df['Hospital Visits Per Year'].fillna(df['Hospital Visits Per Year'].mean())
 
-df['Age'].fillna(df['Age'].mean(),inplace=True)
-df['Hospital Visits Per Year'].fillna(df['Hospital Visits Per Year'].mean(),inplace=True)
+df.loc[:,'Disease']=df['Disease'].fillna("Unknown Disease")
+df.loc[:,'Out-of-Pocket Expenses (Rs)']=df['Out-of-Pocket Expenses (Rs)'].fillna(df['Out-of-Pocket Expenses (Rs)'].mean())
+
+# If the data has a normal distribution (symmetrical), use the mean.
+# If the data has outliers or is skewed, use the median.
+df.loc[:,'Expense Covered by Insurance (%)']=df['Expense Covered by Insurance (%)'].fillna(df['Expense Covered by Insurance (%)'].median())
+df.loc[:,'BMI']=df['BMI'].fillna(df['BMI'].mean())
+
+#converting data type of BMI and Hospital Visit Per Year
+df.loc[:,'BMI']=df['BMI'].astype(int)
+df.loc[:,'Hospital Visits Per Year']=df['Hospital Visits Per Year'].astype(int)
+
+print(df.info())
+
+
+#Patient count with particular disease
+print(df['Disease'].value_counts())
+
+#Age-wise Disease 
+print(df.groupby('Disease')['Age'].value_counts())
+
+
+
+
 
 
 
