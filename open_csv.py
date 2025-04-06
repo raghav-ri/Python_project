d6add4b68d2067691264de392cd34e5352f45d7b
raghav-ri <raghavissa701@gmail.com>
2025-04-06 16:45:09 +0530

    Chart Created

--- open_csv.py
+++ open_csv.py
@@ -1,4 +1,7 @@
 import pandas as pd;
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 df=pd.read_csv(r'C:\Users\RAMAN\Desktop\healthcare_data.csv');
 print(df.info())
 print(df.head())
@@ -91,9 +94,60 @@ print(df.groupby('Disease')['Cause'].value_counts())
 #Average Expense occured during treatment of Disease
 print(df.groupby('Disease')['Out-of-Pocket Expenses (Rs)'].mean())
 
+# hospital preferred in Rural Area and Urban Area 
+print(df.groupby('City Type')['Government Hospital Use'].value_counts())
+
+print(df.info())
+
+#  #   Column                            Non-Null Count  Dtype
+# ---  ------                            --------------  -----
+#  0   ID                                2999 non-null   int64
+#  1   Name                              2999 non-null   object
+#  2   Age                               2999 non-null   float64
+#  3   Gender                            2999 non-null   object
+#  4   State                             2999 non-null   object
+#  5   City Type                         2999 non-null   object
+#  6   Disease                           2999 non-null   object
+#  7   Cause                             2999 non-null   object
+#  8   Hospital Visits Per Year          2999 non-null   float64
+#  9   Out-of-Pocket Expenses (Rs)       2999 non-null   float64
+#  10  Expense Covered by Insurance (%)  2999 non-null   float64
+#  11  Government Hospital Use           2999 non-null   object
+#  12  BMI                               2999 non-null   float64
+
+#Box plot for detecting outlier in Insurance Cover
+sns.boxplot(y='Expense Covered by Insurance (%)', data=df, color='salmon')
+plt.show()
 
 
+#Box plot for detecting outlier in age
+sns.boxplot(y='Age',data=df,color='red')
+plt.show()
 
+#Disease Count
+df['Disease'].value_counts().head(10).plot(kind='bar',color='skyblue')
+plt.show()
+
+
+#Check how BMI is distributed among patients.
+plt.hist(df['BMI'],bins=20,color='mediumseagreen',edgecolor='black')
+plt.title("Distribution Of BMI")
+plt.xlabel("BMI")
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
+
+#Show how different diseases vary across city types.
+pd.crosstab(df['Disease'],df['City Type']).plot(kind='bar',stacked=True )
+plt.show()
+
+#Show how different diseases vary across city types.
+df['Government Hospital Use'].value_counts().plot.pie(autopct="%1.1f",color=['red','gold'])
+plt.show()
+
+#Heatmap
+sns.heatmap(df.corr(numeric_only=True), annot=True, cmap='coolwarm')
+plt.show()
 
 
 
